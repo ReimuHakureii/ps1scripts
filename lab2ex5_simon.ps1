6240308bf8a5f04541fe6a5b77dbd3b403df2af8
ReimuHakureii <63125235+ReimuHakureii@users.noreply.github.com>
2025-02-06 10:51:59 -0500

    Path correction

--- lab2ex5_simon.ps1
+++ lab2ex5_simon.ps1
@@ -1,4 +1,4 @@
 # Exercice 5 : Démarrage en arrière-plan
-Start-Job -ScriptBlock { & "C:\Users\kurom\lab2ex4_simon.ps1" }
+Start-Job -ScriptBlock { & "C:\Users\kurom\downloads\lab2ex4_simon.ps1" }
 # Voir les jobs en cours : Get-Job
 # Arrêter un job : Stop-Job -Id <ID>
