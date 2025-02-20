e66fac7c6d4337fc0a3f881820e14ec506c0c044
ReimuHakureii <63125235+ReimuHakureii@users.noreply.github.com>
2025-02-20 09:09:23 -0500

    Exercice 1 changement commentaires

--- lab3/lab3ex1_simon.ps1
+++ lab3/lab3ex1_simon.ps1
@@ -1,15 +1,15 @@
 # Demande de saisie du nom de l'usager
 $UserName = Read-Host "Entrez le nom d'usager à rechercher"
 
-# On utilise Get-LocalUser pour récupérer le compte local correspondant.
+# Utilisation Get-LocalUser pour récupérer le compte local correspondant
 $user = Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue
 
-# Si l'usager n'est pas trouvé, on affiche un message d'erreur.
+# Quand l'usager n'est pas trouvé on affiche un message d'erreur
 if ($null -eq $user) {
     Write-Host "Usager non trouvé !"
 }
 else {
-    # Affichage du nom de l'usager et de la date/heure de sa dernière connexion.
+    # Affichage du nom de l'usager et de la date/heure de sa dernière connexion
     Write-Host "Usager: $($user.Name)"
     Write-Host "Dernière connexion: $($user.LastLogon)"
 }
