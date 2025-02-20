1b980a973a8b3b6ddd9d7fe1286533cae1faf24c
ReimuHakureii <63125235+ReimuHakureii@users.noreply.github.com>
2025-02-20 09:14:39 -0500

    Exercice 2

---
+++ lab3/lab3ex2_simon.ps1
@@ -0,0 +1,25 @@
+# Demande de saisie des informations à l'utilisateur
+$UserName    = Read-Host "Entrez le nom d'usager"
+$FullName    = Read-Host "Entrez le nom complet"
+$Description = Read-Host "Entrez la description"
+
+# Conversion de la chaîne tge en objet SecureString pour le mot de passe
+$password = ConvertTo-SecureString "tge" -AsPlainText -Force
+
+# Création du compte local
+New-LocalUser -Name $UserName `
+              -FullName $FullName `
+              -Description $Description `
+              -Password $password `
+              # Le mot de passe ne va pas expirer
+              -PasswordNeverExpires `
+              # Le compte n'expire jamais
+              -AccountNeverExpires `
+              # L'utilisateur ne peut pas changer son mot de passe
+              -UserMayNotChangePassword
+
+# Ajout du nouvel usager au groupe "Utilisateurs".
+Add-LocalGroupMember -Group "Utilisateurs" -Member $UserName
+
+# Message de confirmation.
+Write-Host "Compte '$UserName' créé et ajouté au groupe Utilisateurs."
